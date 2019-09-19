eac35042851e5b1f45f96ef957c847d53a0f5799
Yoni Slot <yonislot@gmail.com>
2019-09-19 11:39:13 +0200

    ass1 changes

--- Lab3/yoni/Lab3.hs
+++ Lab3/yoni/Lab3.hs
@@ -5,14 +5,20 @@ import System.Random
 import Test.QuickCheck
 import Lecture3
 
+-- Time spent - slightly under an hour
+-- not even a single eval should succeed
 contradiction :: Form -> Bool
 contradiction = not . satisfiable
 
+-- they should all succeed, therefore, loop over all possiblities an all should be true
 tautology :: Form -> Bool
-tautology f = all (\ v -> evl v f) (allVals f)
+tautology f = all (\v -> evl v f) (allVals f)
 
+-- For all value combinations for form1, whatever form 1 returns should be valid for form 2
 entails :: Form -> Form -> Bool
-entails f1 f2 = all (\v -> evl v f1 == evl v f2) (allVals f1)
+entails f1 f2 = all (\v -> if (evl v f1) then (evl v f2) else True) (genVals (propNames f1 ++ propNames f2))
 
+-- Just like entails, but vice versa as well
 equiv :: Form -> Form -> Bool
 equiv f1 f2 = entails f1 f2 && entails f2 f1
+
