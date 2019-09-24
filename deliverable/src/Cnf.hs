cfca3ccd0f34eb979481facecda62a2a4a753555
Descalon <info@nicoglas.com>
2019-09-24 15:47:21 +0200

    updated cnf

--- deliverable/src/Cnf.hs
+++ deliverable/src/Cnf.hs
@@ -1,28 +1,18 @@
 module Cnf where
     import Lib
 
-    toNnf :: Form -> Form
-    toNnf = arrowfree . nnf
-
-    u = Prop 4
-    v = Prop 5
-
-    val1 = Dsj [p, Cnj [q,r]]
-    val1D = Cnj [Dsj [p, q], Dsj[p,r]]
-
-    val2 = Dsj [p, q, Cnj[r,u,v]]
-    val2D = Cnj [Dsj[p,q,r], Dsj[p,q,u], Dsj[p,q,v]]
-
-    val3 = Dsj[Cnj [p,q], Cnj[r,u]]
-    val3D = Cnj[Dsj[p,r], Dsj[q,r], Dsj[p,u], Dsj[q,u]]
-
-    dsjDistribution :: Form -> Form
-    dsjDistribution (Dsj [x, Cnj fs]) = Cnj $ map (\f -> Dsj [x, dsjDistribution f]) fs
-    dsjDistribution (Dsj [Cnj fs, x]) = Cnj $ map (\f -> Dsj [x, dsjDistribution f]) fs
-    dsjDistribution x = x
-
     cnf :: Form -> Form
-    cnf (Prop a) = Prop a
-    cnf (Neg (Prop a)) = Neg (Prop a)
-    cnf (Cnj xs) = Cnj (map (cnf) xs)
-    cnf (Dsj xs) = cnf $ dsjDistribution (Dsj xs)
+    cnf f = convertToCNF (nnf (arrowfree f))
+    convertToCNF :: Form -> Form
+    -- Remove Brackets from Literals
+    convertToCNF (Prop a) = Prop a
+    convertToCNF (Neg (Prop a)) = Neg (Prop a)
+    -- Remove Double Negatives
+    convertToCNF (Neg (Neg a)) = convertToCNF (nnf (Neg (Neg a)))
+    -- Distribute disjunctions over conjunctions
+    convertToCNF (Dsj [a, (Cnj [b, c])]) = Cnj [(Dsj [(convertToCNF a), b]), (Dsj [(convertToCNF a), c])]
+    -- Distribute conjunctions over disjunctions
+    convertToCNF (Dsj [(Cnj b), a]) = convertToCNF (Dsj [a, (Cnj b)])
+    -- Analyse contents if outermost elements are disjunctions or conjunctions
+    convertToCNF (Dsj [a, b]) = Dsj [(convertToCNF a), (convertToCNF b)]
+    convertToCNF (Cnj [a, b]) = Cnj [(convertToCNF a), (convertToCNF b)]
