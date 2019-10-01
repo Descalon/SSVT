3468aad6920a9a22f081a67e8f5215ba2c10ed50
Descalon <info@nicoglas.com>
2019-10-01 16:33:08 +0200

    golfed symmetric closure

--- deliverable/src/SymmetricClosure.hs
+++ deliverable/src/SymmetricClosure.hs
@@ -1,19 +1,13 @@
 module SymmetricClosure where
+    import Data.List
 
     -- assignment 3 - an hour
     type Rel a = [(a,a)]
 
     symClos :: Ord a => Rel a -> Rel a
     symClos [] = []
-    symClos xs =  uniq $ ssymclose xs -- make sure we don't have duplicates
+    symClos xs =  nub $ xs ++ xs'
+        where xs' = reverseRel xs
 
-    ssymclose :: Ord a => Rel a -> Rel a
-    ssymclose [] = []
-    ssymclose (x:xs) = x : reverseRel x : symClos xs
-
-    uniq :: Eq a => [a] -> [a]
-    uniq [] = []
-    uniq (x:xs) = (if x `elem` xs then id else (x:)) $ uniq xs
-
-    reverseRel :: Ord a => (a, a) -> (a, a)
-    reverseRel (x, y) = (y, x)
+    reverseRel :: Ord a => Rel a -> Rel a
+    reverseRel r = [(y,x) | (x,y) <- r]
